3c4ab4ed8a5edb8d37c1408afff87ff5622e0f0c
Andy <andyvelazquez13@yahoo.com>
2020-12-21 03:34:26 -0800

    added eventListers to all the button, plays the proper sound, and letter to buttons

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,16 +1,71 @@
 
 // document.querySelector("button").addEventListener("click", handleClick);
 
-for(var i = 0; i < 7; i++)
+for(var i = 0; i < document.querySelectorAll("button").length; i++)
 {
-  document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+  var button = document.querySelectorAll("button")[i].innerText
+  // document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+
+  switch (button) {
+    case "w":
+      document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var audio = new Audio("Sound/tom-1.mp3");
+        audio.play();
+      });
+      break;
+    case "a":
+      document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var audio = new Audio("Sound/tom-2.mp3");
+        audio.play();
+      });
+      break;
+    case "s":
+      document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var audio = new Audio("Sound/tom-3.mp3");
+        audio.play();
+      });
+      break;
+    case "d":
+      document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var audio = new Audio("Sound/tom-4.mp3");
+        audio.play();
+      });
+      break;
+    case "j":
+      document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var audio = new Audio("Sound/snare.mp3");
+        audio.play();
+      });
+      break;
+    case "k":
+      document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var audio = new Audio("Sound/crash.mp3");
+        audio.play();
+      });
+      break;
+    case "l":
+      document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var audio = new Audio("Sound/kick-bass.mp3");
+        audio.play();
+      });
+      break;
+    default:
+
+  }
 }
 
 
-function handleClick() {
-  alert("I got clicked");
+function handleClick(audioFile) {
+  var audio = new Audio(audioFile);
+  audio.play();
 }
 
+  switch (button) {
+    case "w":
+      break;
+    default:
+
+  }
 
 
 /* Notes - anonymous function

54a92a1ceddd904b2b1c4e36850443d3bf985d4a
Andy <andyvelazquez13@yahoo.com>
2020-12-30 02:44:05 -0800

    added a press class for animation. add animation to buttons

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,52 +1,16 @@
 
 for(var i = 0; i < document.querySelectorAll("button").length; i++) {
+  // adding an event listener on our drum pictures(these prictures are buttons)
   document.querySelectorAll("button")[i].addEventListener("click", handleClick);
 }
 
-function handleClick() {
-  var buttonInnerHtml = this.innerHTML;
-  switch (buttonInnerHtml) {
-    case "w":
 
-      var audio = new Audio("Sound/tom-1.mp3");
-      audio.play();
-    break;
-    case "a":
-      var audio = new Audio("Sound/tom-2.mp3");
-      audio.play();
-
-    break;
-    case "s":
-      var audio = new Audio("Sound/tom-3.mp3");
-      audio.play();
-
-    break;
-    case "d":
-
-      var audio = new Audio("Sound/tom-4.mp3");
-      audio.play();
-    break;
-    case "j":
-      var audio = new Audio("Sound/snare.mp3");
-      audio.play();
-
-    break;
-    case "k":
-      var audio = new Audio("Sound/crash.mp3");
-      audio.play();
-    break;
-    case "l":
-      var audio = new Audio("Sound/kick-bass.mp3");
-      audio.play();
-    break;
-    default: console.log(buttonInnerHtml);
-
-  }
-}
-
+/* adding an event listener on keypress to the entire document */
 document.addEventListener("keydown", function (event) {
   var keyPressed = event.key;
 
+  //add animation to our key
+  addAnimation(keyPressed);
   switch (keyPressed) {
     case "w":
 
@@ -86,6 +50,58 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
+function handleClick() {
+  var buttonInnerHtml = this.innerHTML;
+
+  //add animation to our click
+  addAnimation(buttonInnerHtml);
+
+  switch (buttonInnerHtml) {
+    case "w":
+
+      var audio = new Audio("Sound/tom-1.mp3");
+      audio.play();
+    break;
+    case "a":
+      var audio = new Audio("Sound/tom-2.mp3");
+      audio.play();
+
+    break;
+    case "s":
+      var audio = new Audio("Sound/tom-3.mp3");
+      audio.play();
+
+    break;
+    case "d":
+
+      var audio = new Audio("Sound/tom-4.mp3");
+      audio.play();
+    break;
+    case "j":
+      var audio = new Audio("Sound/snare.mp3");
+      audio.play();
+
+    break;
+    case "k":
+      var audio = new Audio("Sound/crash.mp3");
+      audio.play();
+    break;
+    case "l":
+      var audio = new Audio("Sound/kick-bass.mp3");
+      audio.play();
+    break;
+    default: console.log(buttonInnerHtml);
+
+  }
+}
+
+function addAnimation(current) {
+
+  var activeButton = document.querySelector("." + current);
+  activeButton.classList.add("pressed");
+
+
+}
 /* Notes - anonymous function
 
   document.querySelector("button").addEventListener("click", function () {

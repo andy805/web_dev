ab958d2ff5e1ec658072b6f353eecb5dad7ae675
Andy <andyvelazquez13@yahoo.com>
2020-12-31 06:45:36 -0800

    chaining method and animation on event listener

--- jQuery/index.js
+++ jQuery/index.js
@@ -7,3 +7,15 @@ $(document).keypress(function (event) {
   var key = event.key;
   $("h1").html(key);
 });
+
+/* animation functions for jQuery */
+$("button").on("click", function () {
+  //animate needs a css property that is a numeric value, will
+  // not work with values that are strings
+  $("h1").animate({opacity:0.5});
+})
+
+/* chaining methods */
+$("button").on("click", function () {
+  $("h1").slideUp().slideDown().animate({opacity:1});
+})

c8c7f335dbe0a1283671d7654b7cc9577dd71394
Andy <andyvelazquez13@yahoo.com>
2021-03-15 21:27:12 -0700

    added post request

--- email-signup/app.js
+++ email-signup/app.js
@@ -5,15 +5,24 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 const port = 3000;
 
 app.get("/", function(req, res) {
-
   console.log("get request");
   res.sendFile(__dirname+"/index.html");
-
 });
+
+app.post("/", function(req, res) {
+  var firstName = req.body.userFirstName;
+  var lastName = req.body.userLastName;
+  var email = req.body.userEmail;
+  console.log("post request");
+  console.log(String(firstName));
+  console.log(String(lastName));
+  console.log(String(email));
+})
 
 app.listen(port, function() {
   console.log("server is running on port "+String(port));

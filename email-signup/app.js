626fc0fd1c6c8fa72991e33db7f14955683eebc2
Andy <andyvelazquez13@yahoo.com>
2021-03-15 19:13:34 -0700

    set up server

---
+++ email-signup/app.js
@@ -0,0 +1,13 @@
+
+/* require express */
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const port = 3000;
+
+app.listen(port, function() {
+  console.log("server is running on port "+String(port));
+});

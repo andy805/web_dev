6e25541053d9d3679c36160cccfb3867832b42ab
Andy <andyvelazquez13@yahoo.com>
2021-03-21 10:39:36 -0700

    added function to subscribe to mailchimp

--- email-signup/app.js
+++ email-signup/app.js
@@ -13,21 +13,23 @@ mailchimp.setConfig({
   server: config.server
 });
 
-async function run() {
-  const response = await mailchimp.ping.get();
-  console.log(response);
-}
+// async function run() {
+//   const response = await mailchimp.ping.get();
+//   console.log(response);
+// }
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 const port = 3000;
 
 app.get("/", function(req, res) {
   console.log("get request");
-  res.sendFile(__dirname+"/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res) {
@@ -38,9 +40,25 @@ app.post("/", function(req, res) {
   console.log(String(firstName));
   console.log(String(lastName));
   console.log(String(email));
+  var data = {
+    fName: firstName,
+    lName: lastName,
+    email: email
+  }
+  async function run() {
+    const response = await mailchimp.lists.addListMember(config.listID, {
+      email_address: data.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: data.fName,
+        LNAME: data.lName
+      }
+    });
+    console.log(response);
+    console.log("user id" + response.id);
+  }
   run();
 })
-
 app.listen(port, function() {
-  console.log("server is running on port "+String(port));
+  console.log("server is running on port " + String(port));
 });

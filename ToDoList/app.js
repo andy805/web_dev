e7eb52ce707d557ad467f5ac6e8afd427329ded4
Andy <andyvelazquez13@yahoo.com>
2021-04-03 16:09:52 -0700

    new styling, new ejs logic to show list

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -2,14 +2,29 @@
 
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 const port = 3000;
+
+var toDoList = [];
 
 app.get("/", function(req, res) {
 
   console.log("get request");
+  var options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  }
+  var today = new Date();
   var dayAsNumber = new Date().getDay();
   var day = "";
+  var testDay = today.toLocaleDateString("en-Us", options);
   switch(dayAsNumber){
     case 0:
       day = "sunday";
@@ -35,8 +50,28 @@ app.get("/", function(req, res) {
     default:
       day = "Error"
   }
-  res.render('list', {dayOfWeek: day});
+  res.render('list', {dayOfWeek: testDay,
+                      toDo: toDoList});
   // res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function(req, res) {
+
+  console.log(req.body.addToDo);
+  console.log("Post request");
+  var options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  }
+  var today = new Date();
+  var newToDoItem = req.body.addToDo;
+  toDoList.push(newToDoItem);
+  console.log("lenght"+ toDoList.length);
+  console.log(toDoList);
+  res.render('list', {dayOfWeek: today.toLocaleDateString("en-Us", options), toDo: toDoList});
 
 });
 
